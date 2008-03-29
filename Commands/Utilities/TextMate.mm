1fe3c9586635bab0a1cbf84522e4c6406ce1e311
Ciarán Walsh <github@ciaranwal.sh>
2008-03-29 15:25:34 +0000

    Returning to the previous usage of OakTextView methods for inserting text
    git-svn-id: http://svn.textmate.org/branches/WIP/Tools/Dialog2@9235 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Commands/Utilities/TextMate.mm
+++ Commands/Utilities/TextMate.mm
@@ -26,19 +26,25 @@
 @implementation TextMate
 + (void)insertText:(NSString*)text asSnippet:(BOOL)asSnippet
 {
-	// ProcessSerialNumber psn = [[NSWorkspace sharedWorkspace] processSerialNumberForApplicationWithIdentifier:@"com.macromates.textmate"];
-	ProcessSerialNumber psn = { 0, kCurrentProcess }; // Could use this once inside TM?
+	id textView = nil;
+	if(asSnippet && (textView = [NSApp targetForAction:@selector(insertSnippetWithOptions:)]))
+		[textView insertSnippetWithOptions:[NSDictionary dictionaryWithObject:text forKey:@"content"]];
+	else if(textView = [NSApp targetForAction:@selector(insertText:)])
+		[textView insertText:text];
 
-	NSAppleEventDescriptor *targetDesc = [NSAppleEventDescriptor descriptorWithDescriptorType:'psn ' bytes:&psn length:sizeof(psn)];
-	NSAppleEventDescriptor *descriptor = [NSAppleEventDescriptor appleEventWithEventClass:'ISTR'
-                                                                                 eventID:'ISTR'
-                                                                        targetDescriptor:targetDesc
-                                                                                returnID:kAutoGenerateReturnID
-                                                                           transactionID:kAnyTransactionID];
-
-	[descriptor setDescriptor:[NSAppleEventDescriptor descriptorWithString:text] forKeyword:'----'];
-	[descriptor setDescriptor:[NSAppleEventDescriptor descriptorWithEnumCode:asSnippet ? 'yes ' : 'no  '] forKeyword:'SNIP'];
-
-	AESendMessage([descriptor aeDesc], NULL, kAENoReply, kAEDefaultTimeout);
+	// // ProcessSerialNumber psn = [[NSWorkspace sharedWorkspace] processSerialNumberForApplicationWithIdentifier:@"com.macromates.textmate"];
+	// ProcessSerialNumber psn = { 0, kCurrentProcess }; // Could use this once inside TM?
+	// 
+	// NSAppleEventDescriptor *targetDesc = [NSAppleEventDescriptor descriptorWithDescriptorType:'psn ' bytes:&psn length:sizeof(psn)];
+	// NSAppleEventDescriptor *descriptor = [NSAppleEventDescriptor appleEventWithEventClass:'ISTR'
+	//                                                                                  eventID:'ISTR'
+	//                                                                         targetDescriptor:targetDesc
+	//                                                                                 returnID:kAutoGenerateReturnID
+	//                                                                            transactionID:kAnyTransactionID];
+	// 
+	// [descriptor setDescriptor:[NSAppleEventDescriptor descriptorWithString:text] forKeyword:'----'];
+	// [descriptor setDescriptor:[NSAppleEventDescriptor descriptorWithEnumCode:asSnippet ? 'yes ' : 'no  '] forKeyword:'SNIP'];
+	// 
+	// AESendMessage([descriptor aeDesc], NULL, kAENoReply, kAEDefaultTimeout);
 }
 @end
