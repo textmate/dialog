f696635aa3cbe6209634b8474b2da687af9b05cd
Allan Odgaard <git@abetarda.com>
2008-09-16 13:46:12 +0000

    • use new argument parsing system
    The syntax is now: "$DIALOG" x-insert --text «text»/--snippet «snippet».
    
    
    git-svn-id: http://svn.textmate.org/branches/WIP/Tools/Dialog2@10566 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Commands/insert.mm
+++ Commands/insert.mm
@@ -13,27 +13,11 @@
 
 - (void)handleCommand:(CLIProxy*)proxy;
 {
-	NSString* text = nil;
+	NSDictionary* args = [proxy parameters];
 
-	if([proxy numberOfArguments] > 3)
-	{
-		ErrorAndReturn(@"too many arguments");
-	}
-	else if([proxy argumentAtIndex:2])
-	{
-		text = [proxy argumentAtIndex:2];
-	}
-	else
-	{
-		NSData *data = [[proxy inputHandle] readDataToEndOfFile];
-
-		if([data length] > 0)
-			text = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
-	}
-
-	if(text == nil || [text length] == 0)
-		ErrorAndReturn(@"no text given");
-
-	insert_snippet(text);
+	if(NSString* text = [args objectForKey:@"text"])
+		insert_text(text);
+	else if(NSString* snippet = [args objectForKey:@"snippet"])
+		insert_snippet(snippet);
 }
 @end
